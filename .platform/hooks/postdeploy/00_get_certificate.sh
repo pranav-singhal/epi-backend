7b4fbe359ae9c818c132079c2fabd8c03f6d68cd
pranavsinghal <pranavsinghal96@gmail.com>
2023-01-08 00:02:01 +0530

    added correct config for india deployment

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d wallet-api.consolelabs.in --nginx --agree-tos --email arvind@consolelabs.in
+sudo certbot -n -d epi-api-c.consolelabs.in --nginx --agree-tos --email arvind@consolelabs.in
