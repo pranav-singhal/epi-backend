9fe94747010ae3dcdcd13521317873a8230253fd
pranavsinghal <pranavsinghal96@gmail.com>
2023-03-20 22:49:53 +0530

    added toString to amount to fix numeric fault err

--- api/services/Web3Service.js
+++ api/services/Web3Service.js
@@ -1,13 +1,13 @@
-const {ethers} = require('ethers');
-const crypto = require('crypto')
+const { ethers } = require('ethers');
+const crypto = require('crypto');
 
 const wallet = new ethers.Wallet(
   _.get(sails, 'config.contractadminwallet.pvtkey'),
 );
 
-const generateNonce = () => crypto.randomBytes(16).toString('hex')
+const generateNonce = () => crypto.randomBytes(16).toString('hex');
 
-const mintMsgHash = (recipient, amount, newNonce) => ethers.utils.solidityKeccak256(['address', 'string', 'uint256'], [recipient, newNonce, amount]);
+const mintMsgHash = (recipient, amount, newNonce) => ethers.utils.solidityKeccak256(['address', 'string', 'uint256'], [recipient, newNonce, amount.toString()]);
 
 module.exports = {
   generateSignedTransaction: async (_senderAddress, _amount) => {
@@ -15,9 +15,9 @@ module.exports = {
     const unsignedMsgHash = mintMsgHash(_senderAddress, _amount, nonce);
     let messageHashBytes = ethers.utils.arrayify(unsignedMsgHash);
 
-    const signedMsgHash = await wallet.signMessage(messageHashBytes)
+    const signedMsgHash = await wallet.signMessage(messageHashBytes);
 
-    return {unsignedMsgHash, signedMsgHash, nonce};
+    return { unsignedMsgHash, signedMsgHash, nonce };
 
   }
 };
