da4584f0a074e537df9300ba5e2a5847ca9a7cc0
pranavsinghal <pranavsinghal96@gmail.com>
2023-03-21 16:44:32 +0530

    removed parseInt for handling large numbers when generating signature

--- api/controllers/PayoutController.js
+++ api/controllers/PayoutController.js
@@ -8,7 +8,7 @@ module.exports = {
 
   generateSignatureForTransaction: async (req, res) => {
     const senderAddress = _.get(req, 'body.senderAddress');
-    const amount = parseInt(_.get(req, 'body.amount'));
+    const amount = _.get(req, 'body.amount');
 
     const signedTransactionResponse = await Web3Service.generateSignedTransaction(senderAddress, amount);
     return res.json(signedTransactionResponse);
