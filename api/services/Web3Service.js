1df5128c06a50bad48ba68ac9cd028cd4f3e38a4
pranavsinghal <pranavsinghal96@gmail.com>
2023-06-24 15:37:18 +0530

    added flatten logic

--- api/services/Web3Service.js
+++ api/services/Web3Service.js
@@ -14,6 +14,35 @@ const generateNonce = () => crypto.randomBytes(16).toString('hex');
 
 const mintMsgHash = (recipient, amount, newNonce) => ethers.utils.solidityKeccak256(['address', 'string', 'uint256'], [recipient, newNonce, amount.toString()]);
 
+const flattenObj = (ob) => {
+
+  // The object which contains the
+  // final result
+  let result = {};
+
+  // loop through the object "ob"
+  for (const i in ob) {
+
+    // We check the type of the i using
+    // typeof() function and recursively
+    // call the function again
+    if ((typeof ob[i]) === 'object' && !Array.isArray(ob[i])) {
+      const temp = flattenObj(ob[i]);
+      for (const j in temp) {
+
+        // Store temp in result
+        result[i + '.' + j] = temp[j];
+      }
+    }
+
+    // Else store ob[i] in result directly
+    else {
+      result[i] = ob[i];
+    }
+  }
+  return result;
+};
+
 module.exports = {
   generateSignedTransaction: async (_senderAddress, _amount, _chain) => {
     const nonce = generateNonce();
@@ -27,7 +56,11 @@ module.exports = {
   },
 
   validateSignedPayload: async (signature, payload, address) => {
-    const stringifiedPayload = JSON.stringify(payload);
+    // payload is flattened to remove nesting. It is then ordered so that signature can be verified
+    // if payload is not ordered and flattened, same payload will generate a different strigified value,
+    // and thus a different signature on client and server
+    const flattenedPayload = flattenObj(payload);
+    const stringifiedPayload = JSON.stringify(flattenedPayload, Object.keys(flattenedPayload).sort());
     const signerAddr = await ethers.utils.verifyMessage(stringifiedPayload, signature);
     return signerAddr === address;
   },
